2e7ea2bb3e91dcf1114cc2c64a5d8537a6f49eb4
Shivam Anand <shivam.anand@fareportal.com>
2022-02-26 02:16:16 +0530

    Hooks CRUD

--- employeeassign/src/HooksImplementation/UpdateComponent.js
+++ employeeassign/src/HooksImplementation/UpdateComponent.js
@@ -1,92 +1,78 @@
-// import { useState, useEffect } from 'react';
-// import axios from 'axios';
-// // import GetHook from './AccountApiGetWithHooks';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+// import GetHook from './AccountApiGetWithHooks';
 
-// export default function PutHook(props) {
-//     debugger;
-//     var [employeeid, setemployeeid] = useState(props.dataSend ?  props.dataSend.employeeid : "");
-//     var [employeename, setemployeeName] = useState(props.dataSend ? props.dataSend.employeename : "");
-//     var [age, setage] = useState(props.dataSend ? props.dataSend.age : "");
-//     var [salary, setsalary] = useState(props.dataSend ? props.dataSend.salary : "");
+export default function PutHook(props) {
+    debugger;
+    var [employeeid, setemployeeid] = useState(props.dataSend ?  props.dataSend.employeeid : "");
+    var [employeename, setemployeeName] = useState(props.dataSend ? props.dataSend.employeename : "");
+    var [age, setage] = useState(props.dataSend ? props.dataSend.age : "");
+    var [salary, setsalary] = useState(props.dataSend ? props.dataSend.salary : "");
     
-//     // var [accountNumber, setNumber] = useState("");
-//     // var [customerName, setName] = useState("");
-//     // var [currentAddress, setAddress] = useState("");
-//     // var [currentBalance, setBalance] = useState("");
+    
+    const onidChange = e => {
+        setemployeeid(e.target.value);
+    };
 
-//     // var [data, setData] = useState([]);
+    const onNameChange = e => {
+        setemployeeName(e.target.value);
+    };
 
-//     // useEffect(() => {
-//     //     setNumber(props.dataSend.accountNumber);
-//     //     setName(props.dataSend.customerName);
-//     //     setAddress(props.dataSend.currentAddress);
-//     //     setBalance(props.dataSend.currentBalance);
-//     //     debugger;
-//     // }, [])
+    const onageChange = e => {
+        setage(e.target.value);
+    };
 
-//     const onNumberChange = e => {
-//         setemployeeid(e.target.value);
-//     };
+    const onsalaryChange = e => {
+        setsalary(e.target.value);
+    };
 
-//     const onNameChange = e => {
-//         setemployeeName(e.target.value);
-//     };
+    const handleSubmit = e => {
+        e.preventDefault();
+        const data = {
+            employeeid,
+            employeename ,
+            age,
+            salary,
+    };
 
-//     const onAddressChange = e => {
-//         setage(e.target.value);
-//     };
+        const requestOptions = {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify(data)
+        };
 
-//     const onBalanceChange = e => {
-//         setsalary(e.target.value);
-//     };
+        axios
+          .post("https://localhost:5001/api/Employee/", data)
+          .then(res => console.log(res))
+          .catch(err => console.log(err));
+    };
 
-//     const handleSubmit = e => {
-//         e.preventDefault();
-//         const data = {
-//             employeeid,
-//             employeename ,
-//             age,
-//             salary,
-//     };
+    return (
+        <div className="EmployeeAddComponent">
+            <form className="EmployeeAddComponent" onSubmit={handleSubmit}>
+               
+                <input
+                    placeholder={props.dataSend.employeeId} value={employeeid}
+                    onChange={onidChange} required />
+                <br />
 
-//         const requestOptions = {
-//             method: "POST",
-//             headers: { "Content-Type": "application/json" },
-//             body: JSON.stringify(data)
-//         };
+                <input
+                    placeholder={props.dataSend.employeename} value={employeename}
+                    onChange={onNameChange} required />
+                <br />
 
-//         axios
-//           .post("https://localhost:5001/api/Employee/", data)
-//           .then(res => console.log(res))
-//           .catch(err => console.log(err));
-//     };
+                <input
+                    placeholder={props.dataSend.age} value={age}
+                    onChange={onageChange} required />
+                <br />
 
-//     return (
-//         <div className="AccountAddComponent">
-//             <form className="AccountAddComponent" onSubmit={handleSubmit}>
-//                 {/* <label value="Account Number"></label> */}
-//                 <input
-//                     placeholder={props.dataSend.employeeId} value={employeeid}
-//                     onChange={onNumberChange} required />
-//                 <br />
+                <input
+                    placeholder={props.dataSend.salary} value={salary}
+                    onChange={onsalaryChange} required />
+                <br />
 
-//                 <input
-//                     placeholder={props.dataSend.employeename} value={employeename}
-//                     onChange={onNameChange} required />
-//                 <br />
-
-//                 <input
-//                     placeholder={props.dataSend.age} value={age}
-//                     onChange={onAddressChange} required />
-//                 <br />
-
-//                 <input
-//                     placeholder={props.dataSend.salary} value={salary}
-//                     onChange={onBalanceChange} required />
-//                 <br />
-
-//                 <button type="submit">Add Student </button>
-//             </form>
-//         </div>
-//     );
-// }
+                <button type="submit">Add Employee </button>
+            </form>
+        </div>
+    );
+}
